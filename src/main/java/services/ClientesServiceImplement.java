b73eb83c515b4668bf0d53480d1da4f82e1a05d0
maicol10borja <166356848+maicol10borja@users.noreply.github.com>
2024-11-21 12:20:27 -0500

    Update ClientesServiceImplement.java

--- src/main/java/services/ClientesServiceImplement.java
+++ src/main/java/services/ClientesServiceImplement.java
@@ -9,7 +9,7 @@ public class ClientesServiceImplement implements ClientesService{
 
     @Override
     public List<Clientes> listar() {
-        return Arrays.asList(new Clientes(1L,"Ivan","Cabrera",1125,"Masculino"),
+        return Arrays.asList(new Clientes(1L,"Michael","Borja",1125,"Masculino"),
                 new Clientes(10L, "Maria", "Gonzalez", 3456, "Femenino"),
                 new Clientes(15L, "Luis", "Perez", 7890, "Masculino"),
                 new Clientes(20L, "Ana", "Ramirez", 1234, "Femenino"),
